2766bc38314cd224a86b961ade15a77dfdb4a2c5
CamilleLeyrer <79653368+CamilleLeyrer@users.noreply.github.com>
2021-11-10 15:12:13 +0100

    ex Numbers done

--- 1_Basics/2_numbers.js
+++ 1_Basics/2_numbers.js
@@ -12,19 +12,19 @@
 // Update 11.07.2021 Population in Luzern did not change according to Wikipedia.
 
 // a. Compute (18 + 107) / (5 * 25)
-
+a = (18 + 107) / (5 * 25);
 console.log(a);
 
 // b. Compute the square root of one million.
-
+b = Math.sqrt(1000000)
 console.log(b);
 
 // c. Take the remainder of the division betwen 123 and 9 squared, minus 1.
-
+c = 123%(9**2) -1;
 console.log(c);
 
 // d. Take the integer part of the float number 2.123456789 (need to use Math).
-
+d = Math.floor(2.123456789)
 console.log(d);
 
 // Now compute:
@@ -52,8 +52,8 @@ console.log(a*b*c*d);
 // and with a upper case for the first letter of every next word. So:
 // luzernPopulation
 // is probably a good candidate. But the final choice is yours!
-luzernPopulation = a*b*c*d;
-console.log(luzernPopulation);
+allTogether = a*b*c*d;
+console.log(allTogether);
 
 // EXERCISE 3. Random numbers.
 //////////////////////////////
@@ -61,10 +61,11 @@ console.log(luzernPopulation);
 // a. Generate a random number between 0 and 1, and store its value
 // in a variable (and pick a proper name for the variable!).
 // Hint. The Math object is your friend.
-
-
+randomVariable = Math.random()
+console.log(randomVariable)
 // b. Generate a random number between 0 and 10.
-
+randomVari2 = Math.random()* 10
+console.log(randomVari2)
 
 
 //////////////////////////////
